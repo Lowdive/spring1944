8f023dc45e59c6e9405aeb45021f45a8b0eafd20
Lowdive <elisantiago.4e@gmail.com>
2015-06-03 12:23:28 -0500

    stubborn commit of modinfo
    even after I gitignored it

--- modinfo.lua
+++ modinfo.lua
@@ -1,10 +1,10 @@
 local modinfo = {
 	name			= 'Spring: 1944',
 	shortName		= 'S44',
-	version			= 'dev-$VERSION',
+	version			= 'dev-$VERSIONL',
 	game			= 'Spring: 1944',
 	shortGame		= 'S44',
-	mutator			= 'Official',
+	mutator			= 'unOfficial-Lows',
 	description		= 'Epic World War II RTS!',
 	url				= 'http://www.spring1944.net/',
 	modtype			= '1',
