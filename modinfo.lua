4f5ca29d8feb0bedc88a482d5ec21ae294a61add
Lowdive <elisantiago.4e@gmail.com>
2020-08-20 09:57:56 -0500

    Determine this as the modified version
    I will work on this clone, and so I will have access to the original clone from master and this clone form master but with my changes as well, so it shows up in slobby as 2 different games

--- modinfo.lua
+++ modinfo.lua
@@ -1,9 +1,9 @@
 local modinfo = {
 	name						= 'Spring: 1944',
-	shortName					= 'S44',
-	version						= '$VERSION',
+	shortName					= 'S44_L',
+	version						= 'L',
 	game						= 'Spring: 1944',
-	shortGame					= 'S44',
+	shortGame					= 'S44_L',
 	mutator						= 'Official',
 	description					= 'Epic World War II RTS!',
 	url							= 'http://www.spring1944.net/',
